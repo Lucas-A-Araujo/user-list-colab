2ee52dece813a8b59b7da3f7dc71133afbe78dbf
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-04-15 12:16:13 -0300

    style: positioning and grouping content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,50 +1,74 @@
 import React, { useState } from 'react';
 
 const Home = () => {
-    const [users, setUsers] = useState([]);
-  
-    const fetchUsers = () => {
-      fetch(`https://randomuser.me/api/?results=20`)
-        .then(response => response.json())
-        .then(data => setUsers(data.results));
-    }
-  
-    return (
-      <>
-        <button onClick={fetchUsers}>Search</button>
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = () => {
+    fetch(`https://randomuser.me/api/?results=20`)
+      .then(response => response.json())
+      .then(data => setUsers(data.results));
+  }
+
+  const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+      maxWidth: '80vw',
+      margin: '0 auto',
+    },
+    cardHeader: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
+    cardContent: {
+      display: 'flex',
+      flexDirection: 'column',
+      minWidth: '100%',
+    },
+    avatar: {
+      width: '20vw',
+      height: '10vh',
+      borderRadius: '50%',
+      marginRight: '20',
+    },
+  };
+
+  return (
+    <>
+      <button onClick={fetchUsers}>Search</button>
+      <div style={styles.container}>
         {users.map(user => (
-          <div key={user.email}>
-            <img src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`} />
+          <div style={styles.cardContent} key={user.email}>
+            <div style={styles.cardHeader}>
+              <img style={styles.avatar} src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`} />
+              <p>
+                {user.name.first} {user.name.last}
+              </p>
+            </div>
             <div>
-                <p>personal info</p>
-              <div>
-                Name: {user.name.first} {user.name.last}
-              </div>
-              <div>
-                E-mail: {user.email}
-              </div>
-              <div>
-                Username: {user.login.username}
-              </div>
-              <div>
-                Age: {user.dob.age}
-              </div>
-              <div>
-                Phone: {user.phone}
-              </div>
-              <div>
-                Call: {user.cell}
-              </div>
+              <p>personal info</p>
+              <p>
+                Username: {user.login.username}<br/>
+                E-mail: {user.email}<br/>
+                Age: {user.dob.age}<br/>
+                Phone: {user.phone}<br/>
+                Cell: {user.cell}
+              </p>
+            </div>
+            <div>
               <p>Location</p>
-              <div>
-                Address: {user.location.street.name}, N° {user.location.street.number}
-              </div>
-              <div>
+              <p>
+                Address: {user.location.street.name}, N° {user.location.street.number}<br/>
                 Location: {user.location.city}, {user.location.state}, {user.location.country}
-              </div>
+              </p>
             </div>
           </div>
         ))}
+      </div>
     </>
   )
 }
