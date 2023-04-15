6da93a75b6d1341ac62e06a27d7b06380549a7fd
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-04-15 10:38:22 -0300

    feat: display relevant user infos

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,50 @@
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
   
     const fetchUsers = () => {
-      fetch(`https://randomuser.me/api/`)
+      fetch(`https://randomuser.me/api/?results=20`)
         .then(response => response.json())
-        .then(data => console.log(data.results));
+        .then(data => setUsers(data.results));
     }
   
     return (
       <>
         <button onClick={fetchUsers}>Search</button>
+        {users.map(user => (
+          <div key={user.email}>
+            <img src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`} />
+            <div>
+                <p>personal info</p>
+              <div>
+                Name: {user.name.first} {user.name.last}
+              </div>
+              <div>
+                E-mail: {user.email}
+              </div>
+              <div>
+                Username: {user.login.username}
+              </div>
+              <div>
+                Age: {user.dob.age}
+              </div>
+              <div>
+                Phone: {user.phone}
+              </div>
+              <div>
+                Call: {user.cell}
+              </div>
+              <p>Location</p>
+              <div>
+                Address: {user.location.street.name}, N° {user.location.street.number}
+              </div>
+              <div>
+                Location: {user.location.city}, {user.location.state}, {user.location.country}
+              </div>
+            </div>
+          </div>
+        ))}
     </>
   )
 }
